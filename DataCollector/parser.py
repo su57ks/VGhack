73b908162b8736854ab0a8472780cf5a9d33c45e
su57ks <samuray.ks@yandex.ru>
2026-04-18 19:50:22 +0300

    removing class

--- DataCollector/parser.py
+++ DataCollector/parser.py
@@ -6,9 +6,9 @@ random = requests.get("https://gram.cerm.ru/?mod=spelling").content
 with codecs.open("random.html", "wb") as f:
     f.write(random)
 
-links = re.search(r"<div id=\"spelling_alphabetical_words_nav\">.+Я</a></div>", random.decode()) #.replace('<div id=\"spelling_alphabetical_words_nav\">', "")) re.findall(r"<a href=\"https://gram.cerm.ru/spelling/.+\">.</a>", 
+data = re.search(r"<div id=\"spelling_alphabetical_words_nav\">.+Я</a></div>", random.decode()) # re.findall(r"<a href=\"https://gram.cerm.ru/spelling/.+\">.</a>", 
 
-links = links.group()
+data = data.group().replace('<div id=\"spelling_alphabetical_words_nav\">', "")
 
-print(len(links))
-print(links)
+print(len(data))
+print(data)
